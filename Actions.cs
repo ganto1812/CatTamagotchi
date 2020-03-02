c2ba59b9b1ae12736b707313d62c1f34b33c65a1
Anto <ganto1812@gmail.com>
2020-03-02 11:21:13 +0000

    Actions enum + Utility random methods

---
+++ Actions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatWebApp
+{
+    public enum HungerAction { None, Milk = 4, Fish = 13, Sardines = 9 }
+    public enum HealthAction { None, Doctor = 14, Massage = 6, Pray = 1 }
+    public enum PlayTimeAction { None, Fetch = 6, Laser = 10, Chase = 8, Wand = 14 }
+    public enum CleanAction { None, Shower = 15, Brush = 7 }
+    public enum SleepAction { None, Nap = 6, Full = 13 }
+
+    public enum ActionLock { None, Hunger, Health, PlayTime, Clean, Sleep}
+
+    public class Actions
+    {
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatWebApp
+{
+    public static class Utility
+    {
+        public static int Clamp(int value, int min = 0, int max = 100)
+        {
+            return Math.Clamp(value, min, max);
+        }
+
+        public static int RandomStartValue()
+        {
+            Random rand = new Random();
+            return rand.Next(80, 100);
+        }
+
+        public static int RandomValue(int min, int max)
+        {
+            Random rand = new Random();
+            return rand.Next(min, max);
+        }
+    }
+}
